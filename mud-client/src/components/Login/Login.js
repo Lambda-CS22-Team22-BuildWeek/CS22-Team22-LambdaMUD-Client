cc82d11d5f9c512f3233e290f7935d1ee55ffc56
chris.j.carter <chris.j.carter91@gmail.com>
2019-10-22 19:26:25 -0600

    Added style components and forms

--- mud-client/src/components/Login/Login.js
+++ mud-client/src/components/Login/Login.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import axios from "axios";
 import config from "../../config/index";
 import { Link } from "react-router-dom";
+import {
+    Form,
+    FormInput,
+    FormLabel,
+    FormSubmitButton,
+    FormHeader,
+} from "../Custom/Forms";
 
 class Login extends Component {
     constructor() {
@@ -47,6 +54,35 @@ class Login extends Component {
         return(
             <Body>
                 <Form onSubmit={this.handleSubmit}>
+                    <FormHeader>MUD Login</FormHeader>
+
+                    <FormLabel name="Username">
+                        <FormInput
+                            onChange={this.handleInput}
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            value={this.state.username}
+                            />
+                    </FormLabel>
+
+                    <FormLabel name="password">
+                        <FormInput 
+                        onChange={this.handleInput}
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        />
+                    </FormLabel>
+
+                    <FormSubmitButton type="submit" disabled={!this.state.password}>
+                        Login
+                    </FormSubmitButton>
+
+                    <Link to="/register">
+                        Not registered yet?
+                    </Link>
 
                 </Form>
             </Body>
--- mud-client/src/components/Register/Register.js
+++ mud-client/src/components/Register/Register.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import axios from "axios";
 import config from "../../config/index";
 import { Link } from "react-router-dom";
+import {
+    Form,
+    FormInput,
+    FormLabel,
+    FormSubmitButton,
+    FormHeader,
+} from "../Custom/Forms";
 
 class Register extends Component {
     constructor() {
@@ -44,6 +51,35 @@ class Register extends Component {
         return (
             <Body>
                 <Form onSubmit={this.handleSubmit}>
+
+                    <FormHeader>MUD Register</FormHeader>
+
+                    <FormLabel name="username">
+                        <FormInput
+                        onChange={this.handleInput}
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        value={this.state.username}
+                        />
+                    </FormLabel>
+
+                    <FormLabel name="password">
+                        <FormInput
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        />
+                    </FormLabel>
+
+                    <FormSubmitButton type="submit" disabled={!this.state.password}>
+                        Register
+                    </FormSubmitButton>
+
+                    <Link to="/login">
+                        Already Registered?
+                    </Link>
 
                 </Form>
             </Body>
